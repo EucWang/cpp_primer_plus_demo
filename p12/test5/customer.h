d602f29ea9b715ee7218bac8b763a7548141f6c1
euc.wang <qqwwdr@gmail.com>
2019-03-23 15:05:18 +0800

    p12 队列的例子

---
+++ p12/test5/customer.h
@@ -0,0 +1,18 @@
+#ifndef CUSTOMER_H_
+#define CUSTOMER_H_
+
+#include <iostream>
+
+class Customer{
+    private:
+    long arrive;
+    int processtime;
+    public:
+    Customer(){arrive = processtime = 0;}
+    void set(long when);
+    long when() const {return arrive;}
+    int ptime() const {return processtime;}
+    friend std::ostream & operator<<(std::ostream & os, Customer & c);
+};
+
+#endif
